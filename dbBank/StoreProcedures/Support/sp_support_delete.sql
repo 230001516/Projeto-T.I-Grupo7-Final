bb0bb6f3e2e9c5520a034d63a986be7f6adb919a
230000424 <230000424@esg.ipsantarem.pt>
2024-12-22 01:24:13 +0000

    Home Controller add

---
+++ dbBank/StoreProcedures/Support/sp_support_delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[sp_support_delete]
+	@id_ticket INT
+AS
+BEGIN
+	DELETE
+	FROM dbo.Support
+	WHERE id_ticket = @id_ticket
+END
---
+++ dbBank/StoreProcedures/Support/sp_support_update.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[sp_support_update]
+    @id_ticket INT ,
+    @id_user INT, 
+    @supName VARCHAR(50),
+    @email VARCHAR(100),
+    @subject VARCHAR(50),
+    @message VARCHAR(600)
+
+AS
+BEGIN
+    UPDATE dbo.Support
+    SET [id_user] = @id_user, [supName] = @supName, [email] = @email, [subject] = @subject, [message] = @message
+    WHERE [id_ticket] = @id_ticket
+END
